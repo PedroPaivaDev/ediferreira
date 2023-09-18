050f591d11d945607fe1e7131d8c426787e91bf3
PedroPaivaDev <pedropaivadev@gmail.com>
2023-09-18 15:39:23 -0300

    fix: uninstalled the 'encoding' lib as it did not solve the problem and returned the 'Admin' page as it was in commit 'cb58c2888498eb989d7ece2bfb239472cad5a05c', which was the last successful deployment;

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -37,75 +37,67 @@ const Admin = () => {
     setPotoFile(files[0]);
     setPhotoPreview(videoPreviewURL);
   }
-
-  if(!userAuth) {
-    return <main className='pt-16'><SignIn /></main>
-  } else {
-    return (
-      <main className={`py-16 ${contentDB ? 'opacity-100' : 'opacity-0'} duration-1000`}>
-        <SignIn />
-        {contentDB && userAuth.email==="contato.ediferreira@gmail.com" ?
-          <div className='w-full flex flex-col justify-start items-start gap-10 max-w-4xl px-20'>
-            <p>Bem vinda, Edi.</p>
-            <div id='homeContent' className='w-full flex flex-col justify-start items-start gap-5'>
-              <h3>Conteúdo da Home</h3>
-              <div className='flex flex-col justify-start items-start gap-3'>
-                <label htmlFor='newBgVideo'>Escolha um novo vídeo vertical com no máximo 5 megabytes e que não possua áudio:</label>
-                <input
-                  onChange={onVideoSelected}
-                  name="newBgVideo"
-                  type="file"
-                  id="newBgVideo"
-                  accept="video/*"
-                  className='text-xs overflow-hidden max-w-[280px]'
+  
+  return (
+    <main className={`py-16 ${contentDB ? 'opacity-100' : 'opacity-0'} duration-1000`}>
+      <SignIn />
+      {contentDB && userAuth && userAuth.email==="contato.ediferreira@gmail.com" &&
+        <div className='w-full flex flex-col justify-start items-start gap-10 max-w-4xl px-20'>
+          <p>Bem vinda, Edi.</p>
+          <div id='homeContent' className='w-full flex flex-col justify-start items-start gap-5'>
+            <h3>Conteúdo da Home</h3>
+            <div className='flex flex-col justify-start items-start gap-3'>
+              <label htmlFor='newBgVideo'>Escolha um novo vídeo vertical com no máximo 5 megabytes e que não possua áudio:</label>
+              <input
+                onChange={onVideoSelected}
+                name="newBgVideo"
+                type="file"
+                id="newBgVideo"
+                accept="video/*"
+                className='text-xs overflow-hidden max-w-[280px]'
+              />
+              {videoPreview && (
+                <video id="videoPreview" autoPlay={true} loop={true} muted={true} playsInline={true} preload="auto"
+                  className='max-w-lg'
+                >
+                  <source src={videoPreview} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              )}
+            </div>
+            <TextArea id='callAbout' label={`Chamada para a página "Sobre":`} placeholder={contentDB.home.callAbout}/>
+            <TextArea id='callProjects' label={`Chamada para a página "Projetos":`} placeholder={contentDB.home.callProjects}/>
+          </div>
+          <div id='aboutContent' className='w-full flex flex-col justify-start items-start gap-5'>
+            <h3>Conteúdo da Sessão "Sobre"</h3>
+            <TextArea id='aboutText' label={`Texto da sessão "sobre":`} placeholder={contentDB.about.text}/>
+            <div className='flex flex-col justify-start items-start gap-3'>
+              <label htmlFor="photoEdi">Escolha uma nova foto para a sessão "sobre"</label>
+              <input
+                onChange={onPhotoSelected}
+                name="photoEdi"
+                type="file"
+                id="photoEdi"
+                accept="image/*"
+                className="text-xs overflow-hidden max-w-[280px]"
+              />
+              {photoPreview && (
+                <Image id="videoPreview" src={photoPreview} alt='Foto Nova da Edi'
+                  width={240} height={240} className='max-w-lg'
                 />
-                {videoPreview && (
-                  <video id="videoPreview" autoPlay={true} loop={true} muted={true} playsInline={true} preload="auto"
-                    className='max-w-lg'
-                  >
-                    <source src={videoPreview} type="video/mp4" />
-                    Your browser does not support the video tag.
-                  </video>
-                )}
-              </div>
-              <TextArea id='callAbout' label={`Chamada para a página "Sobre":`} placeholder={contentDB.home.callAbout}/>
-              <TextArea id='callProjects' label={`Chamada para a página "Projetos":`} placeholder={contentDB.home.callProjects}/>
+              )}
             </div>
-            <div id='aboutContent' className='w-full flex flex-col justify-start items-start gap-5'>
-              <h3>Conteúdo da Sessão "Sobre"</h3>
-              <TextArea id='aboutText' label={`Texto da sessão "sobre":`} placeholder={contentDB.about.text}/>
-              <div className='flex flex-col justify-start items-start gap-3'>
-                <label htmlFor="photoEdi">Escolha uma nova foto para a sessão "sobre"</label>
-                <input
-                  onChange={onPhotoSelected}
-                  name="photoEdi"
-                  type="file"
-                  id="photoEdi"
-                  accept="image/*"
-                  className="text-xs overflow-hidden max-w-[280px]"
-                />
-                {photoPreview && (
-                  <Image id="videoPreview" src={photoPreview} alt='Foto Nova da Edi'
-                    width={240} height={240} className='max-w-lg'
-                  />
-                )}
-              </div>
-            </div>
-            <div id='servicesContent' className='w-full flex flex-col justify-start items-start gap-5'>
-              <h3>Conteúdo da Sessão "Serviços"</h3>
-              <TextArea id='servicesTextFirstP' label={`Primeiro texto:`} placeholder={contentDB.about.servicesText.firstParagraph}/>
-              <TextArea id='servicesTextSecondP' label={`Segundo texto:`} placeholder={contentDB.about.servicesText.secondParagraph}/>
-              <TextArea id='servicesTextThirdP' label={`Terceiro texto:`} placeholder={contentDB.about.servicesText.thirdParagraph}/>
-            </div>
-          </div> :
-          <section className='h-screen flex flex-col gap-5'>
-            <p className='text-status-error'>O e-mail que você selecionou não é o "contato.ediferreira@gmail.com".</p>
-            <p>Clique em "Logout" e escolha o e-mail correto.</p>
-          </section>
-        }
-      </main>
-    );
-  }
+          </div>
+          <div id='servicesContent' className='w-full flex flex-col justify-start items-start gap-5'>
+            <h3>Conteúdo da Sessão "Serviços"</h3>
+            <TextArea id='servicesTextFirstP' label={`Primeiro texto:`} placeholder={contentDB.about.servicesText.firstParagraph}/>
+            <TextArea id='servicesTextSecondP' label={`Segundo texto:`} placeholder={contentDB.about.servicesText.secondParagraph}/>
+            <TextArea id='servicesTextThirdP' label={`Terceiro texto:`} placeholder={contentDB.about.servicesText.thirdParagraph}/>
+          </div>
+        </div>
+      }
+    </main>
+  );
 }
 
 export default Admin;
