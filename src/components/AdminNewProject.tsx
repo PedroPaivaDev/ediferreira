49d6d2288e87cb9e5a56593a1ab102a917dfec19
PedroPaivaDev <pedropaivadev@gmail.com>
2023-10-06 12:42:53 -0300

    feat: 'AdminNewProject' component uses the 'handleFilesSelect' function to select multiple photos of the new project on the computer and view them, so that one of them is chosen as the project's cover;

--- src/components/AdminNewProject.tsx
+++ src/components/AdminNewProject.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 
 import handleTextSubmit from '@/helpers/handleTextSubmit';
+import objectBgImage from '@/helpers/objectBgImage';
 
 import InputText from './InputText';
 import TextArea from './TextArea';
 import Button from './Button';
-import AdminMediaPhotos from './AdminMediaPhotos';
 
 const AdminNewProject = () => {
   const [newProjectName, setNewProjectName] = React.useState<string>('');
   const [newProjectId, setNewProjectId] = React.useState<string>('');
-  const [photosProjectFiles, setPhotosProjectFiles] = React.useState<FileObjectStorage[]>([]);
+  const [photosObjectFiles, setPhotosObjectFiles] = React.useState<FileObjectStorage[] | null>([]);
   const [mainProjectPhoto, setMainProjectPhoto] = React.useState<FileObjectStorage>();
+
+  function handleFilesSelect(
+    event: React.ChangeEvent<HTMLInputElement>,
+    setPhotosObjectFiles: React.Dispatch<React.SetStateAction<FileObjectStorage[] | null>>
+  ) {
+    const files = event.target.files;
+    if (!files || !files[0]) {
+      return;
+    }
+    const arrayPhotoObjects:FileObjectStorage[] = [];
+    for(let i=0; i < files.length; i++) {
+      arrayPhotoObjects.push({
+        name: files[i].name,
+        folder: 'mainPhoto',
+        url: URL.createObjectURL(files[i])
+      })
+    }
+    setPhotosObjectFiles(arrayPhotoObjects);
+  }
+
+  function handleRemovePhoto() {
+  }
 
   React.useEffect(() => {
     newProjectName && setNewProjectId(newProjectName.replace(/[^\w\s]/gi, '').replace(/\s+/g, '_').toLowerCase());
@@ -37,13 +59,39 @@ const AdminNewProject = () => {
         <TextArea label="Descrição:" name={`projects/${newProjectId}&description`} placeholder=''/>
         <Button label='Salvar Alterações dos Textos'/>
       </form>
+      <input
+        type="file"
+        multiple
+        name={`projects/${newProjectId}&images`}
+        id="newPhotosInput"
+        accept="image/*"
+        onChange={(e) => handleFilesSelect(e, setPhotosObjectFiles)}
+        className="text-xs overflow-hidden mt-5"
+      />
       <div className='w-full flex flex-col sm:flex-row flex-wrap justify-center items-center gap-5'>
-        {mainProjectPhoto && photosProjectFiles.map(photo =>
-          <AdminMediaPhotos key={photo.name}
-            photoUrlDB={mainProjectPhoto.url}
-            usePhotoOnPathDB={`projects/${newProjectId}`}
-            photo={photo} fileStorageFolderPath={`projetos/${newProjectId}`}
-          />
+        {photosObjectFiles && photosObjectFiles.map(photo =>
+          <div key={photo.name} style={objectBgImage(photo.url)}
+            className={`
+              h-60 w-60 text-mood-light flex flex-col justify-end items-center p-5
+              relative group hover:shadow-blackShadowInsetBottom cursor-pointer duration-300
+              ${mainProjectPhoto?.url.includes(photo.url) ? 'shadow-blackShadowInsetBottom' : ''}
+            `}
+          >
+            <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"
+              className="absolute top-5 w-5 fill-mood-light hover:fill-status-error cursor-pointer duration-300"
+              onClick={handleRemovePhoto}
+            >
+              <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
+            </svg>
+            {mainProjectPhoto?.url.includes(photo.url) ?
+              <p>
+                Utilizada
+              </p> :
+              <p className='opacity-0 group-hover:opacity-100' onClick={() => setMainProjectPhoto(photo)}>
+                usar esta
+              </p>
+            }
+          </div>
         )}
       </div>
     </div>
