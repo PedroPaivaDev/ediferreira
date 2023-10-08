51f37389bf5d17c2a5f16a937967844be091db38
PedroPaivaDev <pedropaivadev@gmail.com>
2023-10-08 17:57:42 -0300

    feat: Page 404 for not found;

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <main className='mt-12 p-5 h-almostScreen flex justify-center items-center text-justify'>Ops! Parece que essa página não existe. Use a barra de navegação acima para acessar outras páginas.</main>
+  )
+}
+
+export default NotFound;
--- src/components/InputPhotoFile.tsx
+++ src/components/InputPhotoFile.tsx
@@ -68,6 +68,7 @@ const InputPhotoFile = ({projectId, photosObjectFiles, mainProjectPhoto, setPhot
             >
               <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
             </svg>
+            {photo.size>500000 && <p className='text-status-error text-5xl '>Muito Grande!!!</p>}
             <p>{photo.name}</p>
             <small>{convertBytes(photo.size)}</small>
             {setMainProjectPhoto && <>
